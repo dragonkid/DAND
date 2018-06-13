74e2314b88e68ddb856f914c077d76f08cda92dd
dragonkid <idragonkid@gmail.com>
2018-06-13 22:14:52 +0800

    P3 done

--- P3/data_parser.py
+++ P3/data_parser.py
@@ -1,87 +1,9 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
-import pprint
 import re
 import codecs
 import json
-
-"""
-{
-"id": "2406124091",
-"type: "node",
-"visible":"true",
-"created": {
-          "version":"2",
-          "changeset":"17206049",
-          "timestamp":"2013-08-03T16:43:42Z",
-          "user":"linuxUser16",
-          "uid":"1219059"
-        },
-"pos": [41.9757030, -87.6921867],
-"address": {
-          "housenumber": "5157",
-          "postcode": "60625",
-          "street": "North Lincoln Ave"
-        },
-"amenity": "restaurant",
-"cuisine": "mexican",
-"name": "La Cabana De Don Luis",
-"phone": "1 (773)-271-5176"
-}
-
-You have to complete the function 'shape_element'.
-We have provided a function that will parse the map file, and call the function with the element
-as an argument. You should return a dictionary, containing the shaped data for that element.
-We have also provided a way to save the data in a file, so that you could use
-mongoimport later on to import the shaped data into MongoDB. 
-
-Note that in this exercise we do not use the 'update street name' procedures
-you worked on in the previous exercise. If you are using this code in your final
-project, you are strongly encouraged to use the code from previous exercise to 
-update the street names before you save them to JSON. 
-
-In particular the following things should be done:
-- you should process only 2 types of top level tags: "node" and "way"
-- all attributes of "node" and "way" should be turned into regular key/value pairs, except:
-    - attributes in the CREATED array should be added under a key "created"
-    - attributes for latitude and longitude should be added to a "pos" array,
-      for use in geospacial indexing. Make sure the values inside "pos" array are floats
-      and not strings. 
-- if the second level tag "k" value contains problematic characters, it should be ignored
-- if the second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
-- if the second level tag "k" value does not start with "addr:", but contains ":", you can
-  process it in a way that you feel is best. For example, you might split it into a two-level
-  dictionary like with "addr:", or otherwise convert the ":" to create a valid key.
-- if there is a second ":" that separates the type/direction of a street,
-  the tag should be ignored, for example:
-
-<tag k="addr:housenumber" v="5158"/>
-<tag k="addr:street" v="North Lincoln Avenue"/>
-<tag k="addr:street:name" v="Lincoln"/>
-<tag k="addr:street:prefix" v="North"/>
-<tag k="addr:street:type" v="Avenue"/>
-<tag k="amenity" v="pharmacy"/>
-
-  should be turned into:
-
-{...
-"address": {
-    "housenumber": 5158,
-    "street": "North Lincoln Avenue"
-}
-"amenity": "pharmacy",
-...
-}
-
-- for "way" specifically:
-
-  <nd ref="305896090"/>
-  <nd ref="1719825889"/>
-
-should be turned into
-"node_refs": ["305896090", "1719825889"]
-"""
 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
@@ -100,8 +22,9 @@ def shape_children(children):
             if re.search(problemchars, k):
                 continue
             v = child.attrib['v']
+            k, v = fix_kv(k, v)
             splited_k = k.split(':')
-            if len(splited_k) > 2:
+            if len(splited_k) > 2 or v == 'multipolygon':
                 continue
             elif len(splited_k) == 2:
                 prefix, real_k = splited_k
@@ -119,6 +42,27 @@ def shape_children(children):
         del shaped_children['address']
 
     return shaped_children
+
+
+def fix_kv(k, v):
+    if k == type:
+        if v in ['国槐', 'Cypress']:
+            k = 'species'
+        elif v in ['公园', '应急避难场所']:
+            k = '应急避难场所类型'
+        elif v == 'office':
+            k = 'building'
+        else:
+            pass
+    if k in ['疏散人数（万）', '疏散人数(万人)', '应急避难场所疏散人数万人', '应急避难场所疏散人口万人', '应急避难人数（万人）']:
+        k = 'capacities'
+        v = float(v)
+    if k == '疏散人数':
+        k = 'capacities'
+        v = int(v.strip(' people').replace(',', '')) / 10000
+    if k == 'phone':
+        v = '+86-10-{}'.format(v[-8:])
+    return k, v
 
 
 def shape_element(element):
